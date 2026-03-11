c83e66e34546c1b749ccf08a721657ef19bce5d2
cássio <ricardocassio271@gmail.com>
2026-03-11 11:23:28 -0300

    LP

--- E_ModularizacaoFuncoes/modfun_Lt02_24.java
+++ E_ModularizacaoFuncoes/modfun_Lt02_24.java
@@ -4,10 +4,38 @@
  */
 package E_ModularizacaoFuncoes;
 
-/**
- *
- * @author ricar
- */
-public class modfun_Lt02_24 {
+import javax.swing.JOptionPane;
+
+/*
+   24. Receba um valor inteiro. Verifique e mostre se é divisível por 2 e 3.
+   Programador: Cássio
+   Data: 11/03/2026
+*/
+public class modfun_Lt02_24 
+{
+    public static void main(String args[])
+    {
+        int x = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro:"));
+        
+        if (calcular(x))
+        {
+            JOptionPane.showMessageDialog(null, "O número " + x + " é divísivel por 2 e 3!");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "O número " + x + " não é divísivel por 2 e 3!");
+        }
+    }
     
+    static boolean calcular(int num)
+    {
+        if ((num % 2 == 0) && (num % 3 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
--- E_ModularizacaoFuncoes/modfun_Lt02_25.java
+++ E_ModularizacaoFuncoes/modfun_Lt02_25.java
@@ -4,10 +4,66 @@
  */
 package E_ModularizacaoFuncoes;
 
-/**
- *
- * @author ricar
- */
-public class modfun_Lt02_25 {
+import javax.swing.JOptionPane;
+
+/*
+   25. Receba a hora de início e de final de um jogo (HH,MM), calcular o tempo do jogo em horas e minutos, sabendo que o tempo máximo 
+   é menor que 24 horas e pode começar num dia e terminar no outro.
+   Programador: Cássio
+   Data: 11/03/2026
+*/
+public class modfun_Lt02_25 
+{
+    public static void main(String srgs[])
+    {
+        int horaInicial, minInicial, horaFinal, minFinal;
+        
+        boolean valido = false;
+        do
+        {
+            horaInicial = Integer.parseInt(JOptionPane.showInputDialog("Hora de Inicio:"));
+            minInicial = Integer.parseInt(JOptionPane.showInputDialog("Minuto inicial:"));
+            horaFinal = Integer.parseInt(JOptionPane.showInputDialog("Hora final:"));
+            minFinal = Integer.parseInt(JOptionPane.showInputDialog("Minuto final:"));
+            
+            if (!((horaInicial < 0) || (horaInicial > 23) || (minInicial < 0) || (minInicial > 23) 
+              || (horaFinal < 0) || (horaFinal > 23) || (minFinal < 0) || (minFinal > 23)))
+            {
+                valido = true;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "DADOS INVÁLIDOS!");
+            }
+        }
+        while(valido == false);
+        
+        String resultado = calcular(horaInicial, minInicial, horaFinal, minFinal);
+        
+        JOptionPane.showMessageDialog(null, resultado);
+    }
     
+    static String calcular(int horaInicial, int minInicial, int horaFinal, int minFinal)
+    {
+        int inicio = ((horaInicial * 60) + minInicial);
+        int fim = ((horaFinal * 60) + minFinal);
+        
+        int duracao = (fim - inicio);
+        
+        if (duracao < 0)
+        {
+            duracao += (24 * 60);
+        }
+        
+        if ((duracao == 0) || (duracao == 24))
+        {
+            return ("A partida deve durar mais que 0 e menos que 24 horas!");
+        }
+        else
+        {
+            int horas = (duracao / 60);
+            int minutos = (duracao % 60);
+            return ("A partida durou " + horas + " horas e " + minutos + " minutos!");
+        }
+    }
 }
